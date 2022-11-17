76a592db4ec078eb2e5d9f31dd82bc2a12bfa807
Nick Sorros <nsorros@gmail.com>
2022-11-17 08:10:04 +0000

    Add aws

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 sudo apt update
 
 sudo apt install direnv
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 echo "# Direnv" >> ~/.bashrc
 echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
