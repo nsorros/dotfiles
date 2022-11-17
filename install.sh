49777b0ae151ab59bb228cb37dcf55794d370eb4
Nick Sorros <nsorros@gmail.com>
2022-11-17 09:11:17 +0000

    Add activate function

--- install.sh
+++ install.sh
@@ -5,6 +5,21 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
+echo "" >> ~/.bashrc
 echo "# Direnv" >> ~/.bashrc
 echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+echo "" >> ~/.bashrc
+
+echo "# Activate" >> ~/.bashrc
+echo "function activate {" >> ~/.bashrc
+echo "    if [[ -d .env ]]; then" >> ~/.bashrc
+echo "        source .env/bin/activate" >> ~/.bashrc
+echo "    elif [[ -d .venv ]]; then" >> ~/.bashrc
+echo "        source .venv/bin/activate" >> ~/.bashrc
+echo "    elif [[ -d venv ]]; then" >> ~/.bashrc
+echo "        source venv/bin/activate" >> ~/.bashrc
+echo "    elif [[ -d build/virtualenv ]]; then" >> ~/.bashrc
+echo "        source build/virtualenv/bin/activate" >> ~/.bashrc
+echo "    fi" >> ~/.bashrc
+echo "}" >> ~/.bashrc
 echo "" >> ~/.bashrc
